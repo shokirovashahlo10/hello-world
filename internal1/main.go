e8d76de0a7eb85bc23bdb2a5855daa29c5208282
Shahlo Shokirova <shokirovashahlo10@gmail.com>
2026-05-06 21:20:19 +0500

    fix: save changes before merge

--- internal1/main.go
+++ internal1/main.go
@@ -1,1 +1,14 @@
 package main
+
+import "awesomeProject3/internal1/product"
+
+func main() {
+	phone := product.Product{
+		ID:     1,
+		Name:   "Xiaomi Redmi Note 14 6/128 ГБ",
+		Price:  2499000,
+		Months: 12,
+	}
+
+	product.PrintProduct(phone)
+}
--- internal1/product/product_test.go
+++ internal1/product/product_test.go
@@ -1,1 +1,16 @@
 package product
+
+import "fmt"
+
+func ExampleMonthlyPayment() {
+	p := Product{
+		ID:     101,
+		Name:   "Xiaomi Redmi Note 14 6/128 ГБ",
+		Price:  2499000,
+		Months: 12,
+	}
+
+	payment := MonthlyPayment(p)
+	fmt.Println(payment)
+
+}
